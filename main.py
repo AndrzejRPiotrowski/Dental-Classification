4a087017b3e942c7b919d5fbdc8511edbca05145
SirAnen <andrzejrpiotrowski@gmail.com>
2023-03-01 21:26:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import streamlit as st
-from detectron2.engine import DefaultPredictor
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
-from detectron2.utils.video_visualizer import VideoVisualizer
-from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog
-from detectron2.utils.visualizer import ColorMode, Visualizer
-from detectron2 import model_zoo
 from PIL import Image
-import tqdm
 import tensorflow_hub as hub
 import tensorflow as tf
 import numpy as np
@@ -74,52 +64,7 @@ st.markdown("Granty Norweskie 2023 - Norway Grants 2023")
          
 #st.date_input(label, value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
 #st.camera_input(label, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
-
-def func_1(x):
-    detector = Detector(model_type=x)
-    image_file = st.file_uploader("Upload An Image",type=['png','jpeg','jpg'])
-    if image_file is not None:
-        file_details = {"FileName":image_file.name,"FileType":image_file.type}
-        st.write(file_details)
-        img = Image.open(image_file)
-        st.image(img, caption='Uploaded Image.')
-        with open(image_file.name,mode = "wb") as f: 
-            f.write(image_file.getbuffer())         
-        st.success("Saved File")
-        detector.onImage(image_file.name)
-        img_ = Image.open("result.jpg")
-        st.image(img_, caption='Proccesed Image.')
-
-def main():
-    with st.expander("About the App"):
-        st.markdown( '<p style="font-size: 30px;"><strong>Welcome to my Object Detection App!</strong></p>', unsafe_allow_html= True)
-        st.markdown('<p style = "font-size : 20px; color : white;">This app was built using Streamlit, Detectron2 and OpenCv to demonstrate <strong>Object Detection</strong> in both videos (pre-recorded) and images.</p>', unsafe_allow_html=True)
-        
-
-
-    option = st.selectbox(
-     'What Type of File do you want to work with?',
-     ('Images', 'Videos'))
-
-    #st.write('You selected:', option)
-    if option == "Images":
-        st.title('Object Detection for Images')
-        st.subheader("""
-This takes in an image and outputs the image with bounding boxes created around the objects in the image.
-""")
-        func_1('objectDetection')
-    else:
-        st.title('Object Detection for Videos')
-        st.subheader("""
-This takes in a video and outputs the video with bounding boxes created around the objects in the video.
-""")
-        func_2('objectDetection')
-
-
-if __name__ == '__main__':
-		main()
-          
-          
+         
 def predict(image):
     classifier_model = 'newclass04.h5'
     model=keras.models.load_model(classifier_model)
@@ -196,6 +141,5 @@ if class_btn:
             time.sleep(1)
             st.success('ANALIZA ZAKOŃCZONA SUKCESEM.')
             st.write(predictions)
-
 
 
